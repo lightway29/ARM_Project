f19d91fe4eb2a28c04d9ed8647d24977afaf47c5
sameera <sameera0824@gmail.com>
2015-11-04 12:20:55 +0530

    Initial Update

---
+++ Blinky/Flash/retarget.d
@@ -0,0 +1,6 @@
+.\flash\retarget.o: Retarget.c
+.\flash\retarget.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\flash\retarget.o: C:\Keil\ARM\ARMCC\bin\..\include\rt_misc.h
+.\flash\retarget.o: C:\Keil\ARM\ARMCC\bin\..\include\stddef.h
+.\flash\retarget.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\flash\retarget.o: Serial.h
