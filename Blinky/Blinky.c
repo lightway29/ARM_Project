d894332dc207ea415fb7fd95fd455bdf155ecfa5
Lightway <lightway@Lightways-MacBook-Pro.local>
2015-11-04 17:45:09 +0530

    Fixed Unwanted code

--- Blinky/Blinky.c
+++ Blinky/Blinky.c
@@ -1,38 +1,19 @@
-/*----------------------------------------------------------------------------
- * Name:    Blinky.c
- * Purpose: LED Flasher
- * Note(s): possible defines set in "options for target - C/C++ - Define"
- *            __USE_LCD   - enable Output on LCD
- *----------------------------------------------------------------------------
- * This file is part of the uVision/ARM development tools.
- * This software may only be used under the terms of a valid, current,
- * end user licence from KEIL for a compatible version of KEIL software
- * development tools. Nothing else gives you the right to use this software.
- *
- * This software is supplied "AS IS" without warranties of any kind.
- *
- * Copyright (c) 2008-2011 Keil - An ARM Company. All rights reserved.
- *----------------------------------------------------------------------------*/
-
 #include <stdio.h>
 #include "LPC17xx.H"                         /* LPC17xx definitions           */
-
-#include "Serial.h"
 #include "LED.h"
 #include "ADC.h"
 
-                                                                         
-char text[10];
+
 
 /*----------------------------------------------------------------------------
   Main Program
  *----------------------------------------------------------------------------*/
+
 int main (void) {
   uint32_t ad_avg = 0;
   uint16_t ad_val = 0, ad_val_ = 0xFFFF;
 
   LED_Init();                                /* LED Initialization            */
-  SER_Init();                                /* UART Initialization           */
   ADC_Init();                                /* ADC Initialization            */
 
 
@@ -53,12 +34,6 @@ int main (void) {
       }
     }
 
-//    if (ad_val ^ ad_val_) {                  /* AD value changed              */
-//      ad_val_ = ad_val;
-//
-//      sprintf(text, "0x%04X", ad_val);       /* format text for print out     */
-//
-//    }
 
 
 
