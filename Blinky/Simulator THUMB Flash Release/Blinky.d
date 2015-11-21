6248d597d4160633932d608d0f41aaa129f04fa0
Lightway <lightway@Lightways-MacBook-Pro.local>
2015-11-21 21:11:26 +0530

    commit for changing files

---
+++ Blinky/Simulator THUMB Flash Release/Blinky.d
@@ -0,0 +1,12 @@
+Simulator\ THUMB\ Flash\ Release/Blinky.o: \
+ /Users/lightway/ARM_Project/Blinky/./Blinky.c \
+ /Applications/CrossWorks\ for\ ARM\ 3.5/include/stdio.h \
+ /Applications/CrossWorks\ for\ ARM\ 3.5/include/__crossworks.h \
+ /Users/lightway/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/LPC1000/include/CMSIS/LPC17xx.H \
+ /Users/lightway/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/CMSIS_3/CMSIS/Include/core_cm3.h \
+ /Applications/CrossWorks\ for\ ARM\ 3.5/include/stdint.h \
+ /Users/lightway/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/CMSIS_3/CMSIS/Include/core_cmInstr.h \
+ /Users/lightway/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/CMSIS_3/CMSIS/Include/core_cmFunc.h \
+ /Users/lightway/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/LPC1000/include/CMSIS/system_LPC17xx.h \
+ /Users/lightway/ARM_Project/Blinky/./Stepper.h \
+ /Users/lightway/ARM_Project/Blinky/./ADC.h
